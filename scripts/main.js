4c8f7ed251043c5e788a4e270432610e0fc6a1e0
SunShine214 <sunshineprojects214@gmail.com>
2024-06-20 14:09:22 -0600

    basic (and disgusting) line collision. Still need to not check the same pair twice, only x is checked. Additionally, max and min shouldnt be used. Probably a much better method than slope regardless, though...

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ var mouse_pos = [0, 0];
 
 //TODO BLAST EM GONE
 var test_tmp = new PhysObject(10, [0, 0], [100, 100], new Collider([100, 100], [150, 150]));
-load()
+load();
 
 //this is disgusting
 document.addEventListener("keydown", (event) => {console.log(event.key); if ((!event.isComposing) && event.key === "Escape") end()})
@@ -20,12 +20,21 @@ document.addEventListener("mousedown", (event) => {clicking = true});
 document.addEventListener("mouseup", (event) => {clicking = false; item[0].set_accel([0, 0])});
 
 function load() {
-    item = [new DrivableObject(10, [3, 3], [50, 50]), document.getElementById("runner-canvas")]
+    item = [new DrivableObject(10, [0, 0], [50, 50], new Collider([150, 100], [150, 150])), document.getElementById("runner-canvas")]
     pids = [new pid(5, 2, 0.1), new pid(5, 2, 0.1)];
 }
 
 async function process() {
     while (loop) {
+        //Partial copypaste from https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineTo
+        var canvas = document.getElementById('test-canvas');
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath(); // Start a new path
+        ctx.moveTo(...test_tmp.collider.position1); 
+        ctx.lineTo(...test_tmp.collider.position2); 
+        ctx.stroke(); 
+
         //much more disgusting
         item[1].style.setProperty('top', item[0].position[1].toString()); 
         item[1].style.setProperty('left', item[0].position[0].toString());
--- scripts/physics.js
+++ scripts/physics.js
@@ -9,6 +9,7 @@ class Collider {
         this.position1 = position1;
         this.position2 = position2;
     }
+    //TODO: change to all realitive pos, also default link collider pos to its parent body
     get_normal(collided) {
         //do cool maths
         return [1, 1];
@@ -18,6 +19,24 @@ class Collider {
             if (i.collider == null) {
                 continue;
             }
+            var tmp_col_x = Math.abs(i.collider.position1[0] - this.position1[0])
+            var tmp_col_y = Math.abs(i.collider.position1[1] - this.position1[1])
+            var tmp_rel_xdiff = Math.abs(i.collider.position1[0] - i.collider.position2[0])
+            var tmp_rel_ydiff = Math.abs(i.collider.position1[1] - i.collider.position2[1])
+
+            console.log("col x: " + tmp_col_x);
+            console.log("col y: " + tmp_col_y);
+            console.log("rel x: " + tmp_rel_xdiff);
+            console.log("rel y: " + tmp_rel_ydiff);
+            
+            console.log("rel x according to y on slope: " + (Math.max(0, Math.min(tmp_rel_xdiff, tmp_col_y * (tmp_rel_xdiff > 0 ? (tmp_rel_ydiff / tmp_rel_xdiff) : 0))))); 
+
+            if (this.position1[0] >= (Math.max(0, Math.min(tmp_rel_xdiff, tmp_col_y * (tmp_rel_ydiff / tmp_rel_xdiff)))) + i.collider.position1[0]) {
+                console.log("Objects colliding!");
+                colliding_objs.set([this, i], true);
+                continue;
+            }
+            /*
             //do checks, for now if overlap
             if (this.position1[0] >= i.collider.position1[0] || this.position1[1] <= i.collider.position1[1]) {
                 colliding_objs.set([this, i], true); //need to ensure same order for different items (sorting??)
@@ -27,9 +46,10 @@ class Collider {
                 colliding_objs.set([this, i], true);
                 continue;
             }
+            */
 
         }
-        console.log(colliding_objs);
+        //console.log(colliding_objs);
         return colliding_objs;
     }
 }
@@ -48,7 +68,13 @@ class PhysObject {
         this.velocity = new_velo;
     }
     set_pos(new_pos) {
+        //TODO: link collider pos, dont do redundant calcs (have collider.set_pos method)
+        var tmp_diff =  [(this.position[0] - new_pos[0]), (this.position[1] - new_pos[1])];
         this.position = new_pos;
+        if (this.collider != null) {
+            this.collider.position1 = Vector.add_vector(this.collider.position1, tmp_diff);
+            this.collider.position2 = Vector.add_vector(this.collider.position2, tmp_diff);
+        }
     }
 
     collide(collider) {
@@ -58,8 +84,8 @@ class PhysObject {
 }
 
 class DrivableObject extends PhysObject {
-    constructor(mass, velocity, position) {
-        super(mass, velocity, position);
+    constructor(mass, velocity, position, collider = null) {
+        super(mass, velocity, position, collider);
         this.acceleration = [0, 0];
     }
     set_accel(new_accel) {
@@ -72,6 +98,7 @@ function process_phys(objects) {
     for(var i of objects) {
         console.log(i);
         i.set_pos(Vector.add_vector(i.velocity, i.position));
+        if (i.collider != null) i.set_pos(Vector.add_vector(i.velocity, i.position));
         if (i instanceof DrivableObject) {
             i.set_velo(Vector.add_vector(i.velocity, i.acceleration));
         }
